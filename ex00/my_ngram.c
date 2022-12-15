ee582b22af88d9cabe93442c8ca003eee535ff08
Karl <local-part@domain>
2022-12-15 22:07:36 +0100

    [my_ngram/ex00/] my_ngram.c updated: released My n-gram.

--- ex00/my_ngram.c
+++ ex00/my_ngram.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 
+void fill_array( int* array, char* string)
+{
+	int index = 0;
 
-void fill_array(int* array,char* string){
-    int index = 0;
-    while (string[index] != '\0'){
-        array[(int)string[index]]++;
-        index++;
-    }
+	while (string[ index ] != '\0')
+	{
+		array[ (int) string[ index ] ]++;
+		index++;
+	}
 }
 
 #define array_size 178
 
+int main( int argc, char* argv[])
+{
+	int index = 1;
+	int array[ array_size ] = { 0 };
 
-int main(int argc, char* argv[]){
-    int index = 1;
-    int array[array_size] = {0};
+	for ( index = 1; index < argc; index++ )
+	{
+		fill_array(&array[ 0 ], argv[ index ]);
+	}
+	for ( index = 0; index < array_size; index++ )
+	{
+		if ( index != '"' && array[ index ] > 0 )
+		{
+			printf("%c:%d\n", index, array[ index ]);
+		}
+	}
 
-    for (index = 1; index < argc; index++){
-        fill_array(&array[0], argv[index]);
-    }
-    for (index = 0; index < array_size; index++){
-        if (index != '"' && array[index] > 0){
-            printf("%c:%d\n", index, array[index]);
-        }
-    }
-    return(0);
+	return ( 0 );
 }
